01608869a4baf6e9b8c5f4949225a58f2d0f4af5
farhanahraf03 <farhanahraf03@gmail.com>
2021-01-10 00:25:41 +0530

    Day5 almost done

--- Day5Task/logic.js
+++ Day5Task/logic.js
@@ -27,5 +27,65 @@ function getData()
 function deleteData()
 {
     let xhr = new XMLHttpRequest();
+
+    xhr.onload = function()
+    {
+        if(xhr.status == 200)
+        {
+            console.log("Deleted successfully");
+        }
+
+        else
+        {
+            console.log("Deleted unsuccessful");   
+        }
+    };
+
+    xhr.onerror = function()
+    {
+        console.log("Some error occured"); 
+    };
+
+    xhr.open('DELETE','https://apiapptrainingnewapp.azurewebsites.net/api/Products/42');
+    xhr.send();
+
 }
 
+function postData()
+{
+    let xhr = new XMLHttpRequest();
+
+    xhr.onload = function()
+    {
+        if(xhr.status == 201)
+        {
+            console.log("Added successfully");
+        }
+
+        else
+        {
+            console.log("Add unsuccessful");   
+        }
+    };
+
+    xhr.onerror = function()
+    {
+        console.log("Some error occured"); 
+    };
+
+    var data = {
+        BasePrice: 40,
+        CategoryName: 'Stationary',
+        Description: 'Gel-Pen',
+        Manufacturer: 'Reynolds',
+        ProductId: 'Prd911',
+        ProductName: 'Pen',
+    };
+
+    xhr.open('POST','https://apiapptrainingnewapp.azurewebsites.net/api/Products');
+
+    xhr.setRequestHeader("Content-Type","application/json");
+
+    xhr.send(JSON.stringify(data));
+
+}
