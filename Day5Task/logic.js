299b3ebd8c22d0c195dd67be190c47419475f9e2
farhanahraf03 <farhanahraf03@gmail.com>
2021-01-09 23:48:04 +0530

    commit 2

--- Day5Task/logic.js
+++ Day5Task/logic.js
@@ -6,7 +6,26 @@ function getData()
     {
         if(xhr.status == 200)
         {
-            
+            console.log(xhr.response);
         }
-    }
+
+        else
+        {
+            console.log("Error occured whie sending GET request");
+        }
+    };
+
+    xhr.onerror = function()
+    {
+        console.log("Some error occured");
+    };
+
+    xhr.open('GET','https://apiapptrainingnewapp.azurewebsites.net/api/Products');
+    xhr.send();
 }
+
+function deleteData()
+{
+    let xhr = new XMLHttpRequest();
+}
+
