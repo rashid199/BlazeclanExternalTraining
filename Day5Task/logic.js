8289da9732e8053b9a001be2a23b4d1ff0ef3db1
farhanahraf03 <farhanahraf03@gmail.com>
2021-01-10 20:46:13 +0530

    post proxy added

--- Day5Task/logic.js
+++ Day5Task/logic.js
@@ -46,7 +46,7 @@ function deleteData()
         console.log("Some error occured"); 
     };
 
-    xhr.open('DELETE','https://apiapptrainingnewapp.azurewebsites.net/api/Products/42');
+    xhr.open('DELETE','https://apiapptrainingnewapp.azurewebsites.net/api/Products/54');
     xhr.send();
 
 }
@@ -74,15 +74,65 @@ function postData()
     };
 
     var data = {
-        BasePrice: 40,
+        BasePrice: 6900,
         CategoryName: 'Stationary',
         Description: 'Gel-Pen',
         Manufacturer: 'Reynolds',
-        ProductId: 'Prd911',
+        ProductId: 'Prd9111',
         ProductName: 'Pen',
     };
 
+
+    const validateData = {
+        set: function(target, property, value)
+            {
+                console.log(target.property);
+            }
+    };
+
+    const ProxyData = new Proxy(data, validateData);
+    
     xhr.open('POST','https://apiapptrainingnewapp.azurewebsites.net/api/Products');
+
+    xhr.setRequestHeader("Content-Type","application/json");
+
+    xhr.send(JSON.stringify(ProxyData));
+
+}
+
+function putData()
+{
+    let xhr = new XMLHttpRequest();
+
+    xhr.onload = function()
+    {
+        if(xhr.status == 200)
+        {
+            console.log("Updated successfully");
+        }
+
+        else
+        {
+            console.log("Update unsuccessful");   
+        }
+    };
+
+    xhr.onerror = function()
+    {
+        console.log("Some error occured"); 
+    };
+
+    xhr.open('PUT','https://apiapptrainingnewapp.azurewebsites.net/api/Products/229');
+
+    var data = {
+        BasePrice: 69,
+        CategoryName: 'Stationary',
+        Description: 'Gel-Pen',
+        Manufacturer: 'Trimax',
+        ProductId: 'Prd420',
+        ProductName: 'Pen',
+        ProductRowId: 229,
+    };
 
     xhr.setRequestHeader("Content-Type","application/json");
 
