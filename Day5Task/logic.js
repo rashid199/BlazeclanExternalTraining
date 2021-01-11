95cfca04dbc0cf8b6f302ed1f18c6dbaa7eca2a8
farhanahraf03 <farhanahraf03@gmail.com>
2021-01-11 10:53:46 +0530

    "mahesh edit"

--- Day5Task/logic.js
+++ Day5Task/logic.js
@@ -73,30 +73,109 @@ function postData()
         console.log("Some error occured"); 
     };
 
-    var data = {
-        BasePrice: 6900,
-        CategoryName: 'Stationary',
-        Description: 'Gel-Pen',
-        Manufacturer: 'Reynolds',
-        ProductId: 'Prd9111',
-        ProductName: 'Pen',
-    };
+    // var data = {
+    //     BasePrice: 694989,
+    //     CategoryName: 'Stationary',
+    //     Description: 'Gel-Pen',
+    //     Manufacturer: 'Reynolds',
+    //     ProductId: 'Prd9111',
+    //     ProductName: 'Pen',
+    // };
 
 
     const validateData = {
-        set: function(target, property, value)
+        set: function(target, prop, value)
             {
-                console.log(target.property);
+                if(prop === 'ProductId') 
+                {
+                    if(typeof(value) != "string")
+                    {
+                        alert(`Value of ${prop} ain't string type`);
+                        return false;
+                    }
+
+                    else
+                    {
+                        target[prop] = value;
+                        return true;
+                    }
+                }
+
+                if(prop === 'BasePrice') 
+                {
+                    if(typeof(value) != "number")
+                    {
+                        alert(`Value of ${prop} ain't number type`);
+                        return false;
+                    }
+                    else
+                    {
+                        target[prop] = value;
+                        return true;
+                    }
+                }
+
+                if(prop === 'ProductName') 
+                {
+                    if(target[prop].length>30)
+                    {
+                        alert(`Value of ${prop} exceeded 30 characters`);
+                        return false;
+                    }
+
+                    else
+                    {
+                        target[prop] = value;
+                        return true;
+                    }
+                }
+
+                target[prop] = value;
+                console.log(`Value of ${prop} is ${value}`);
+                return true;
+            },
+
+            get: function(target, prop)
+            {
+                console.log(`Getting ${prop} value ${target[prop]}`);
+                return target[prop];
             }
     };
+    var targetData = 
+    {
+         BasePrice: 694989,
+         CategoryName: 'Stationary',
+         Description: 'Gel-Pen',
+         Manufacturer: 'Reynolds',
+         ProductId: 'Prd9111',
+         ProductName: 'Pen',
+    };
 
-    const ProxyData = new Proxy(data, validateData);
+    const ProxyData = new Proxy(targetData, validateData)
+
+    ProxyData.BasePrice = 911420; 
+    ProxyData.CategoryName = 'Toothpaste';
+    ProxyData.Description = 'Toothpaste';
+    ProxyData.Manufacturer = 'Vicco';
+    ProxyData.ProductId = 'Prd100';
+    ProxyData.ProductName = 'Vajradanti';
+    
+    console.log(ProxyData);
+    // console.log(targetData);
+
+    // targetData.BasePrice = ProxyData.BasePrice;
+    // targetData.CategoryName = ProxyData.CategoryName;
+    // targetData.Description = ProxyData.Description;
+    // targetData.Manufacturer = ProxyData.Manufacturer;
+    // targetData.ProductId = ProxyData.ProductId;
+    // targetData.ProductName = ProxyData.ProductName;
     
     xhr.open('POST','https://apiapptrainingnewapp.azurewebsites.net/api/Products');
 
     xhr.setRequestHeader("Content-Type","application/json");
 
-    xhr.send(JSON.stringify(ProxyData));
+    console.log(targetData);
+    //xhr.send(JSON.stringify(targetData));
 
 }
 
