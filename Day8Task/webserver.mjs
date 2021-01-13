afd4afc415a15a00d7be418e64d5f7fa8197b366
farhanahraf03 <farhanahraf03@gmail.com>
2021-01-13 14:01:29 +0530

    Day8 Stuff

---
+++ Day8Task/webserver.mjs
@@ -0,0 +1,77 @@
+import * as http from 'http';
+import * as fs from 'fs';
+
+let ProductsInfo = [
+    {
+        "ProductName":'Vicco Vajradanti',
+        "CategoryName":'Toothpaste',
+        "Price":69,
+        "ProductId":1
+    },
+    {
+        "ProductName":'Parle-G',
+        "CategoryName":'Biscuit',
+        "Price":10,
+        "ProductId":2
+    }
+];
+
+
+let server = http.createServer((req,resp) => {
+
+
+    if(req.method === "POST")
+    {
+        let receivedData;
+        console.log("POST Method Initiated...");
+
+        req.on('data', (d) =>
+        {
+            receivedData = JSON.parse(d);
+
+            ProductsInfo.push(receivedData);
+
+            resp.writeHead(201);
+            //resp.write("<html><body>Record added</body></html>");
+
+            console.log(ProductsInfo);
+
+            //resp.end();
+
+        });
+    }
+
+    if(req.url === '/home')
+    {
+        console.log(req.url);
+
+        fs.readFile('./home.html',(error,file) => 
+        {
+
+            
+            if(error)
+            {
+                resp.writeHead(404, {'Content-Type': 'text/html'});
+                resp.write(error.message);
+                resp.end();
+            }
+
+            console.log(`File read = ${file}`);
+            resp.writeHead(200, {'Content-Type': 'text/html'});
+            resp.write(file); 
+            resp.end();
+
+        });
+    }
+
+    else
+    {
+        resp.writeHead(200, {'Content-Type': 'text/html'});
+        resp.write("file"); 
+        resp.end();
+    }
+    
+});
+
+server.listen(9087);
+console.log(`Listening on port 9087`);
